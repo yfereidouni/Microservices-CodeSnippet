167690af1d0dbf494a0be5638d115d173d10ea27
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-12-04 11:30:07 +0100

    GetPlatforms and GetPlatform was developed.

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+
+namespace PlatformService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    private readonly IPlatformRepo _repository;
+    private readonly IMapper _mapper;
+
+    public PlatformsController(IPlatformRepo repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+        Console.WriteLine("--> Getting Platforms...");
+        
+        var platformItems =  _repository.GetAllPlatforms();
+
+        return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+    }
+
+
+    [HttpGet("{id:int}", Name = "GetPlatformById")]
+    public ActionResult<PlatformReadDto> GetPlatformById(int id)
+    {
+        var platformItem = _repository.GetPlatform(id);
+        if (platformItem !=null)
+        {
+            return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+}
--- PlatformService/Profiles/PlatformsProfile.cs
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -10,6 +10,6 @@ public class PlatformsProfile : Profile
 	{
 		// Source -> Target
 		CreateMap<Platform, PlatformReadDto>(); //.ReverseMap();
-		CreateMap<PlatformCreateDto, Platform>();
+        CreateMap<PlatformCreateDto, Platform>();
 	}
 }
